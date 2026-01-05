7d0677c603eaa654dea45f6aee8a08ac4ab90f75
Katherine Silvestre <katherinesilv@gmail.com>
2026-01-05 18:06:23 +0000

    Add project files.

---
+++ Ex1/Produto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex1
+{
+    internal class Produto
+    {
+        //Atributos
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Stock { get; set; }
+
+        //Construtores
+        //Construtor padrão
+        public Produto()
+        {
+            Stock = 0;
+        }
+        //Construtor com parâmetros
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Stock = quantidade;
+        }
+        //Métodos
+        //Adicionar produtos ao stock
+        public void AdicionarProdutos(int quantidade)
+        {
+            Stock += quantidade;
+        }
+        //Remover produtos do stock
+        public void RemoverProdutos(int quantidade)
+        {
+            Stock -= quantidade;
+        }
+        //Consultar valor total do stock
+        public double ConsultarStock()
+        {
+            return Preco * Stock;
+        }
+        //Sobrescrever método ToString
+        public override string ToString()
+        {
+            return $"Produto: {Nome}, Preço(Un): {Preco:F2}€, Unidades: {Stock}, Valor Total: {ConsultarStock():F2}€";
+        }
+    }
+}
---
+++ Ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Ex1;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Configuração para suportar caracteres Unicode no console
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        // Código do programa principal
+        // Criação de um objeto Produto e interação com o usuário
+        Produto produto = new Produto();
+        Console.WriteLine("Insira os dados do produto:");
+        Console.Write("Nome: ");
+        produto.Nome = Console.ReadLine();
+        Console.Write("Preço: ");
+        produto.Preco = double.Parse(Console.ReadLine()!);
+        Console.Write("Quantidade em estoque: ");
+        produto.Stock = int.Parse(Console.ReadLine()!);
+        Console.WriteLine();
+        Console.WriteLine("Dados do produto: " + produto);
+        Console.WriteLine();
+        // Adição e remoção de produtos do estoque
+        Console.Write("Digite o número de produtos a serem adicionados ao estoque: ");
+        int quantidade = int.Parse(Console.ReadLine()!);
+        produto.AdicionarProdutos(quantidade);
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + produto);
+        Console.WriteLine();
+        Console.Write("Digite o número de produtos a serem removidos do estoque: ");
+        quantidade = int.Parse(Console.ReadLine()!);
+        produto.RemoverProdutos(quantidade);
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + produto);
+        Console.WriteLine();
+    }
+}
